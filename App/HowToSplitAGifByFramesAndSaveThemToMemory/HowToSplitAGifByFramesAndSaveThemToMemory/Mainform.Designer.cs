d6750656006557a43234972bc14dd740dcdf935e
juniair <juniair.kim@gmail.com>
2017-04-07 14:54:10 +0900

    GIF의 이미지를 나누는 프로그램 추가

---
+++ App/HowToSplitAGifByFramesAndSaveThemToMemory/HowToSplitAGifByFramesAndSaveThemToMemory/Mainform.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace HowToSplitAGifByFramesAndSaveThemToMemory
+{
+    partial class Mainform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mainform));
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.lbFrames = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImage
+            // 
+            this.pbImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbImage.Location = new System.Drawing.Point(139, 12);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(350, 241);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbImage.TabIndex = 0;
+            this.pbImage.TabStop = false;
+            // 
+            // lbFrames
+            // 
+            this.lbFrames.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.lbFrames.FormattingEnabled = true;
+            this.lbFrames.Location = new System.Drawing.Point(12, 12);
+            this.lbFrames.Name = "lbFrames";
+            this.lbFrames.Size = new System.Drawing.Size(121, 225);
+            this.lbFrames.TabIndex = 1;
+            this.lbFrames.SelectedIndexChanged += new System.EventHandler(this.lbFrames_SelectedIndexChanged);
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 265);
+            this.Controls.Add(this.lbFrames);
+            this.Controls.Add(this.pbImage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Mainform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GIF Splice";
+            this.Load += new System.EventHandler(this.Mainform_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.ListBox lbFrames;
+    }
+}
+
---
+++ App/HowToSplitAGifByFramesAndSaveThemToMemory/HowToSplitAGifByFramesAndSaveThemToMemory/Mainform.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Reflection;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace HowToSplitAGifByFramesAndSaveThemToMemory
+{
+    /*
+     * pbImage = A PictureBox
+     * lbFiles = A ListBox
+     */ 
+    public partial class Mainform : Form
+    {
+        public Mainform()
+        {
+            InitializeComponent();
+        }
+
+        string pathToImage = @"test.gif";
+        //string pathToImage = @"test.png";
+
+        List<byte[]> frames = new List<byte[]>() { };
+
+        private void Mainform_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //Assuming "test.gif" is in the directory where this application is located
+                pathToImage = AppDomain.CurrentDomain.BaseDirectory + pathToImage;
+
+                //Try extracting the frames
+                frames = EnumerateFrames(pathToImage);
+                if (frames == null || frames.Count() == 0)
+                {
+                    throw new NoNullAllowedException("Unable to obtain frames from " + pathToImage);
+                }
+
+                for (int i = 0; i < frames.Count(); i++)
+                {
+                    lbFrames.Items.Add("Frame-" + i.ToString());
+                }
+
+                lbFrames.SelectedIndex = 0;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Error type: " + ex.GetType().ToString() + "\n" +
+                    "Message: " + ex.Message,
+                    "Error in " + MethodBase.GetCurrentMethod().Name
+                    );
+            }
+        }
+
+        private List<byte[]> EnumerateFrames(string imagePath)
+        {
+            try
+            {
+                //Make sure the image exists
+                if (!File.Exists(imagePath))
+                {
+                    throw new FileNotFoundException("Unable to locate " + imagePath);
+                }
+
+                Dictionary<Guid, ImageFormat> guidToImageFormatMap = new Dictionary<Guid, ImageFormat>()
+                {
+                    {ImageFormat.Bmp.Guid,  ImageFormat.Bmp},
+                    {ImageFormat.Gif.Guid,  ImageFormat.Png},
+                    {ImageFormat.Icon.Guid, ImageFormat.Png},
+                    {ImageFormat.Jpeg.Guid, ImageFormat.Jpeg},
+                    {ImageFormat.Png.Guid,  ImageFormat.Png}
+                };
+
+                List<byte[]> tmpFrames = new List<byte[]>() { };
+
+                using (Image img = Image.FromFile(imagePath, true))
+                {
+                    //Check the image format to determine what
+                    //format the image will be saved to the 
+                    //memory stream in
+                    ImageFormat imageFormat = null;
+                    Guid imageGuid = img.RawFormat.Guid;
+
+                    foreach (KeyValuePair<Guid, ImageFormat> pair in guidToImageFormatMap)
+                    {
+                        if (imageGuid == pair.Key)
+                        {
+                            imageFormat = pair.Value;
+                            break;
+                        }
+                    }
+
+                    if (imageFormat == null)
+                    {
+                        throw new NoNullAllowedException("Unable to determine image format");
+                    }
+
+                    //Get the frame count
+                    FrameDimension dimension = new FrameDimension(img.FrameDimensionsList[0]);
+                    int frameCount = img.GetFrameCount(dimension);
+
+                    //Step through each frame
+                    for (int i = 0; i < frameCount; i++)
+                    {
+                        //Set the active frame of the image and then 
+                        //write the bytes to the tmpFrames array
+                        img.SelectActiveFrame(dimension, i);
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            
+                            img.Save(ms, imageFormat);
+                            tmpFrames.Add(ms.ToArray());
+                        }
+                    }
+
+                }
+
+                return tmpFrames;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Error type: " + ex.GetType().ToString() + "\n" +
+                    "Message: " + ex.Message,
+                    "Error in " + MethodBase.GetCurrentMethod().Name
+                    );
+            }
+
+            return null;
+        }
+
+        private Bitmap ConvertBytesToImage(byte[] imageBytes)
+        {
+            if (imageBytes == null || imageBytes.Length == 0)
+            {
+                return null;
+            }
+
+            try
+            {
+                //Read bytes into a MemoryStream
+                using (MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    //Recreate the frame from the MemoryStream
+                    using (Bitmap bmp = new Bitmap(ms))
+                    {
+                        return (Bitmap)bmp.Clone();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Error type: " + ex.GetType().ToString() + "\n" +
+                    "Message: " + ex.Message,
+                    "Error in " + MethodBase.GetCurrentMethod().Name
+                    );
+            }
+
+            return null;
+        }
+
+        private void ClearPictureBoxImage()
+        {
+            try
+            {
+                if (pbImage.Image != null)
+                {
+                    pbImage.Image.Dispose();
+                }
+                pbImage.Image = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Error type: " + ex.GetType().ToString() + "\n" +
+                    "Message: " + ex.Message,
+                    "Error in " + MethodBase.GetCurrentMethod().Name
+                    );
+            }
+        }
+
+        private void lbFrames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (lbFrames.SelectedIndex == -1)
+                {
+                    return;
+                }
+                
+                //Make sure frames have been extracted
+                if (frames == null || frames.Count() == 0)
+                {
+                    throw new NoNullAllowedException("Frames have not been extracted");
+                }
+
+                //Make sure the selected index is within range
+                if (lbFrames.SelectedIndex > frames.Count() - 1)
+                {
+                    throw new IndexOutOfRangeException("Frame list does not contain index: " + lbFrames.SelectedIndex.ToString());
+                }
+
+                //Clear the PictureBox
+                ClearPictureBoxImage();
+
+                //Load the image from the byte array
+                pbImage.Image = ConvertBytesToImage(frames[lbFrames.SelectedIndex]);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Error type: " + ex.GetType().ToString() + "\n" +
+                    "Message: " + ex.Message,
+                    "Error in " + MethodBase.GetCurrentMethod().Name
+                    );
+            }
+        }
+
+    }
+}
